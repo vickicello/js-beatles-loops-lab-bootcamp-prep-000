ac4a480940de8292fc847727e76bc3e59a4ca540
Vicki Hamm <vicki.cello@gmail.com>
2018-04-11 02:28:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ function theBeatlesPlay(arrayMusicians, arrayInstruments) {
   var array = [];
   var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
   var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  for (musician = 0);
 }
